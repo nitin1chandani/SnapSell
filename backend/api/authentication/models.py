96c1874be72dad8f151441f3d530f1c7d293cbf6
nitin1chandani <nitin1chandani@gmail.com>
2025-03-09 11:02:09 +0530

    added: email as required field

--- backend/api/authentication/models.py
+++ backend/api/authentication/models.py
@@ -3,8 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     ROLE_CHOICES = (
         ("admin", "Admin"),
         ("user", "User"),
     )
     role = models.CharField(max_length=5, choices=ROLE_CHOICES, default="user")
+    REQUIRED_FIELDS = ['email', 'role']
